71e7ca92e129039b9d2116dcfe0bc9efdb882f2d
kingjulian24 <brownjulian2@gmail.com>
2015-12-10 21:14:04 -0500

    somehow I don't need to send a response?

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,19 @@ exports.index = function(req, res){
 };
 
 exports.getData = function(req, res){
-    var userInput = app.processDateRange(req.params);
+    var dateRange = app.processDateRange(req.params);
     
-    if (userInput.inRange) {
-        
-        app.search.db(app.db, userInput, function(err, data){
-            
+    if (!dateRange.err) {
+        app.search.db(dateRange.dates, function(err, data){
            if(err) {
                res.send(err);
            } else {
-               res.json(_.flatten(data));
+               res.json(data); // some how this is not needed ????
            }
         });
         
     } else {
-        res.status(403).json({error: true, message: "The date range cannot be longer than 7 days"});
+        res.status(403).json({error: dateRange.err.toString() });
     }
     
 };
