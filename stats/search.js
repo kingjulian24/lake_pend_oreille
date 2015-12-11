a6cbcbd39e7e8226e906ab4b7c2185513cbac0b6
kingjulian24 <brownjulian2@gmail.com>
2015-12-11 13:30:17 -0500

    refactor

--- stats/search.js
+++ stats/search.js
@@ -3,25 +3,18 @@ var request = require('request');
 var _= require('underscore');
 var db = require('./models/readings');
 
-
-var searchDB = function (dates, cb) {
-    async.map(dates, queryDB, function(err, data){
-        cb(_.flatten(data));
-    });
-  
-};
-
 var queryDB = function(date,cb ) {
-    var missingIDs = [];
      db.wsm.find({ date: date }, function(err,data){
-        if(data.length < 1 ){
-           missingIDs.push(date);  
-        }
-         return cb( null, missingIDs );
+        if(data.length < 1 ) { return cb( null, date ); } // return date not in db
+        cb(null, false);      
      });
  };
 
-
+var searchDB = function (dates, cb) {
+    async.map(dates, queryDB, function(err, data){
+        cb(_.compact(data));
+    });
+};
 
 module.exports = {
     db: searchDB
