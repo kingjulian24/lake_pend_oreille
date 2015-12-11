0ef6917e566682b425a44855a3d2f1e828bc3b4b
kingjulian24 <brownjulian2@gmail.com>
2015-12-11 16:55:55 -0500

    urls to check

--- stats/search.js
+++ stats/search.js
@@ -2,6 +2,7 @@ var async = require('async');
 var request = require('request');
 var _= require('underscore');
 var db = require('./models/readings');
+var moment = require('moment');
 
 var queryDB = function(date,cb ) {
      db.wsm.find({ date: date }, function(err,data){
@@ -11,10 +12,27 @@ var queryDB = function(date,cb ) {
  };
 
 var searchDB = function (dates, cb) {
-    async.map(dates, queryDB, function(err, data){
-        cb(_.compact(data));
+    async.map(dates, queryDB, function(err, dates){
+        var urls = getUrls(_.compact(dates));
+        cb(urls);
     });
 };
+
+function getUrls(dates) {
+    var baseUrl = "http://lpo.dt.navy.mil/data/DM/";
+    var urls = _.map(dates, function(date){
+        var newDate = new Date(date);
+        var md = moment(newDate).format('MM_DD');
+        var year = moment(newDate).format('YYYY');
+        return {
+            wind_speed : baseUrl +""+year+"/"+md+"/Wind_Speed",
+            air_temp : baseUrl +""+year+"/"+md+"/Air_Temp",
+            bar_press : baseUrl +""+year+"/"+md+"/Barometric_Press"
+        };
+    });
+    
+    return urls;
+}
 
 module.exports = {
     db: searchDB
