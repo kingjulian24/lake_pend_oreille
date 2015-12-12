5aa99138885a546d7fbb85fc3d77a5be96421a8d
kingjulian24 <brownjulian2@gmail.com>
2015-12-12 16:47:24 -0500

    fix test

--- test/urls.test.js
+++ test/urls.test.js
@@ -2,24 +2,25 @@ var should = require('should');
 var urls = require('../stats/urls');
 var helpers = require('./helpers');
 
-d1 ="2014/01/01";
-d2 ="2014/01/02";
+var d1 ="2014/01/01";
+var d2 ="2014/01/02";
+var d3 = [new Date(d1).getTime(), new Date(d2).getTime()];
 
 describe('url stats', function(){
 	it('should list urls', function(done){
-		var x = urls(new Date(d1), new Date(d2));
-		var expected =
-		{ air_temp: 
-			 [ 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_01/Air_Temp',
-				 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_02/Air_Temp' ],
-			bar_press: 
-			 [ 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_01/Barometric_Press',
-				 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_02/Barometric_Press' ],
-			wind_speed: 
-			 [ 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_01/Wind_Speed',
-				 'http://lpo.dt.navy.mil/data/DM/2014/2014_00_02/Wind_Speed' ],
-			dates: [ 1388556000000, 1388642400000 ]
-		};
+		var x = urls.getUrls(d3);
+		var expected = [
+               {
+                    air_temp: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_01/Air_Temp',
+                    bar_press: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_01/Barometric_Press',
+                    wind_speed: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_01/Wind_Speed'
+              },
+               {
+                    air_temp: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_02/Air_Temp',
+                    bar_press: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_02/Barometric_Press',
+                    wind_speed: 'http://lpo.dt.navy.mil/data/DM/2014/2014_01_02/Wind_Speed'
+              }
+        ];
 
 		x.should.eql(expected);
 		done();
