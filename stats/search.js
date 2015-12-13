dba7093f3f82aff5840fd857f3d65984cccf6f06
kingjulian24 <brownjulian2@gmail.com>
2015-12-13 02:55:17 -0500

    Done :)

--- stats/search.js
+++ stats/search.js
@@ -22,7 +22,7 @@ var searchDB = function (dates, cb) {
             var records = genDBRecords.generate(readings);
             async.map(records, saveToDB, function(err, results){
                 async.map(queries, getData, function(err, data){
-                    cb(proccessData(data));
+                    cb(processData(data));
                 });
                     
             }); 
@@ -30,6 +30,38 @@ var searchDB = function (dates, cb) {
  
     });
 };
+
+function processData(data) {
+    var pd = {};
+    var rawData = {
+        wind_speed: [],
+        air_temp: [],
+        bar_press: []
+    }
+    var rawData2 = {
+        wind_speed: [],
+        air_temp: [],
+        bar_press: []
+    }
+    for(prop in data){
+        for(var i = 0; i < data[prop].length; i++ ){
+            rawData[prop] = rawData[prop].concat(data[prop][i]);
+        }
+    }
+    
+    for (prop in rawData) {
+        for(var i = 0; i < rawData[prop].length; i++ ){
+            rawData2[prop].push(rawData[prop][i].data);
+        }
+    }
+
+    
+    for(prop in rawData2) {
+        pd[prop] = getStats(rawData2[prop]);
+    }
+    
+    return pd;
+}
 
 var saveToDB = function(record, cb) {
    record.save(function(err){
@@ -87,41 +119,31 @@ function prepDBQuery(dates) {
 
 }
 
-//async.map(x, getData, function(err, data){
-//    
-//});
-//
-//getData(new Date("2014/01/01").getTime(), 'wind_speed', function(err, data){
-//    console.log(data);
-//});
 
-// // Mean
-//        var readings = dash.map(rows, function(row){  return row.data; });
-//        var sum = dash.reduce(readings, function(memo, num){ return memo + num; });
-//        var mean = Math.round((sum/readings.length)*100)/100;
-//        
-//        // Median
-//        readings.sort();
-//        var isOdd = readings.length % 2 === 1 ? true : false;
-//        var median;
-//        if(isOdd){
-//            var index = Math.floor([readings.length/2]);
-//            median = readings[index];
-//        } else {
-//            var index1 = readings.length/2;
-//            var index2 = index1 - 1;
-//            median = (readings[index1] + readings[index2]) / 2;
-//        }
-//        
-//        cb(err,{
-//            type: type,
-//            mean: mean,
-//            median: median
-       // });
+function getStats(readings) {
+    // Mean
+    var sum = dash.reduce(readings, function(memo, num){ return memo + num; });
+    var mean = Math.round((sum/readings.length)*100)/100;
 
-    
+    // Median
+    readings.sort();
+    var isOdd = readings.length % 2 === 1 ? true : false;
+    var median;
+    if(isOdd){
+        var index = Math.floor([readings.length/2]);
+        median = readings[index];
+    } else {
+        var index1 = readings.length/2;
+        var index2 = index1 - 1;
+        median = (readings[index1] + readings[index2]) / 2;
+    }
 
-
+    return {
+        mean: mean,
+        median: median
+    };
+}
+        
 
 module.exports = {
     db: searchDB
