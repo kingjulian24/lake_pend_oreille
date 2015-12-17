e2499233e02db2c99f0bd7848daa99122a47c21d
kingjulian24 <brownjulian2@gmail.com>
2015-12-17 14:30:49 -0500

    Fixed date formats and error messages

--- public/js/myScripts.js
+++ public/js/myScripts.js
@@ -1,12 +1,19 @@
 $(function(){
-    var params,
-         sumChartData = [],
-        rawChartData = [];
     
+    var sumChartData = [],
+        rawChartData = [],
+        errMsg,
+        startDate,
+        endDate;
+    var $loadIndicator = $('#loading');
+    var $errIndicator = $('#error');
+    
+    // summary dat ajax request
     function summary(cb){
         $.ajax({
-            url:'/getData/'+params,
+            url:'/getData/'+startDate.year+'/'+startDate.md+'/'+endDate.md,
             success: function(data){
+                $('#error').addClass('hide');
                 cb(null, data);
             },
            error: function(xhr,status,error) {
@@ -15,34 +22,47 @@ $(function(){
         }); 
     }
     
+    // raw data ajax request
     function raw(cb){
         $.ajax({
-            url:'/getData/'+params+'/raw',
+            url:'/getData/'+startDate.year+'/'+startDate.md+'/'+endDate.md+'/raw',
             success: function(data){
+                $('#error').addClass('hide');
                 cb(null, data);
             },
            error: function(xhr,status,error) {
                cb(error,null);
            }
-        });
+        }); 
     }
     
-    function loadData(p) {
-        params = p;
+    // call ajax requests
+    function loadData() {
         async.series([summary,raw], function(err, data){
-            formatData(data);
-            loadCharts();
+            if(!data[0].error) {
+                formatData(data);
+                loadCharts();
+                showIndicator(false, $loadIndicator);
+                showIndicator(false, $errIndicator);
+            } else if(data[0].error) {
+                errMsg = data[0].error;
+                $errIndicator.html(errMsg);
+                showIndicator(true, $errIndicator);
+            } else {
+                console.log('There was an error: ', err);
+            }
+            
         });
        
     }
     
+    // format ajax data
     function formatData(data) {
         formatSumChartData(data[0]);
         formatRaWChartData(data[1]);
     }
     
-    
-    
+    // format summary chart data
     function formatSumChartData(data) {
         var atMean = ['Mean Air Temperature'];
         var atMedian = ['Median Air Temperature'];
@@ -65,8 +85,7 @@ $(function(){
         sumChartData.push(atMean,atMedian,bpMean,bpMedian,wsMean,wsMedian);
     }
     
-    
-    
+    // format raw chart data
     function formatRaWChartData(data) {
         var myDates = ['x'];
         var types = {
@@ -87,11 +106,13 @@ $(function(){
         rawChartData.push(myDates,types.air_temp,types.wind_speed,types.bar_press);
     }
     
+    // load all charts
     function loadCharts() {
         loadSumChart();
         loadRawChart();
     }
     
+    // load summary chart
     function loadSumChart(){
         var chart = c3.generate({
             bindto: '#sum',
@@ -120,6 +141,7 @@ $(function(){
         });
     }
     
+    // load raw chart
     function loadRawChart(){
         var chart = c3.generate({
             bindto: '#raw',
@@ -132,7 +154,7 @@ $(function(){
               x: {
                 type: 'timeseries',
                 tick: {
-                  format: '%Y-%m-%d %H:%M:%S'
+                  format: '%m-%d-%Y %H:%M:%S'
                 }
               } // x
             }, // axis
@@ -142,18 +164,40 @@ $(function(){
         });
     }
     
+    // format dates for ajax requests
+    function formatDate(date) {
+      var someday = new Date(date);
+      var month = someday.getUTCMonth() + 1;
+      var day = someday.getUTCDate();
+      var year = someday.getUTCFullYear();
+
+      if (month <= 9) { month = '0' + month; }
+      if (day <= 9) { day = '0' + day; }
+
+      return {
+          year: ''+year,
+          md:''+month+'-'+day
+      };
+    }
     
-    
-    loadData('2014/09-05/09-07');
-    
- 
-    
-    
+    function showIndicator(show,type) {
+        if(show){
+            type.removeClass('hide');
+        } else {
+            type.addClass('hide');
+        }
+    }
 
     
-    
-    
-    
-    
+ // Events -------
+    $('form').on('submit',function(e){
+        startDate = formatDate(document.rangeform.from.value);
+        endDate = formatDate(document.rangeform.to.value);
+        loadData();
+        showIndicator(true,$loadIndicator);
+         
+  
+        e.preventDefault();
+    });
     
 });
