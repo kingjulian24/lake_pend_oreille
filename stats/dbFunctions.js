2d7697b188bc0052c83231108d5129d2f2b2efc3
kingjulian24 <brownjulian2@gmail.com>
2015-12-17 18:22:04 -0500

    Refactor to fix genrecords bug

--- stats/dbFunctions.js
+++ stats/dbFunctions.js
@@ -38,25 +38,29 @@ exports.genRecords = function(readingsList) {
     var r = readingsList;
     var records = [];
     var readingArr, dateStr,id;
-    for(var i = 0; i < r.length; i++) {
-        var obj = r[i];
-        for(prop in obj){
-            for (var i = 0; i < obj[prop].length; i++) {
-                readingArr = _.compact(obj[prop][i].trim().split(" "));
+    
+    readingsList.forEach(function(typeObj){
+        for(type in typeObj){
+            for(var i = 0; i < typeObj[type].length; i++) {
+                readingArr = _.compact(typeObj[type][i].trim().split(" "));
                 dateStr = new Date( readingArr[0].replace(/_/g,"/") ).getTime();
                 id = new Date ( readingArr[0].replace(/_/g,"/") +" "+ readingArr[1] ).getTime();
                 records.push(
-                    new db[prop]({
+                    new db[type]({
                         _id: id,
                         date: dateStr,
                         data: parseFloat(readingArr[2])
                     })
                 );
             
-            }
-        } 
-    }
+            } // for var
+            
+        } // for type
+    });
+    
     return records;
 };
+
+
 
 
